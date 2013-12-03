aa89c2097ddf25f142aa449785a49a2f95632422
David Hariri <david.hariri@gmail.com>
2013-12-02 22:13:52 -0500

    undefined

---
+++ index.php
@@ -0,0 +1,38 @@
+<?php 
+$logged = false;
+?>
+<html>
+<head>
+
+	<?php
+	if ($logged == true) {
+		?>
+			<title>Project Connect</title>
+		<?php
+	}
+	else {
+		?>
+			<title>Log In</title>
+		<?php
+	}
+?>
+<script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+</head>
+<body>
+<?php
+	if($logged == true) {
+		include 'views/home.php';
+	}
+	else {
+		include 'views/login.php';
+	}
+?>
+</body>
+<script>
+	function login() {
+		var value = $('.password').val();
+		console.log(value);
+		$('body').load("controllers/login.php?v="+value);
+	}
+</script>
+</html>
---
+++ views/login.php
@@ -0,0 +1,2 @@
+<input class="password" type="text" placeholder="password"></input>
+<button class="enter" onclick="login()">Enter</button>
